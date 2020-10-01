30b1c80f38ba70a427f662ba2eb8201d5f218d8c
mrlne <70581486+Marlenevanerp@users.noreply.github.com>
2020-10-01 14:11:42 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,2 @@
-// JavaScript Document
-var document.querySelector("header button")
-
-function toggleClass(){
-	header.classList.toggle('menu');
-}
-
-menuButton.addEventListener("click", toggleClass);
 
 
