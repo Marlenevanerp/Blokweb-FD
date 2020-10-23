a4692da2dc7e42ed044ce45d5829a0790321dd1c
mrlne <70581486+Marlenevanerp@users.noreply.github.com>
2020-10-23 20:07:48 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
+console.log('script');
+
+var menuButton = document.querySelector ('menu');
+
+var menuList = document.querySelector ('navigatie');
 
 
+
+function toggleMenu(){
+	menuList.classlist.toggle('is-open')
+
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+
+console.log('hoi')
