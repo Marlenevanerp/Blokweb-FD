e476d00c7aac6e333941a712cd7ebb5c02161e04
mrlne <70581486+Marlenevanerp@users.noreply.github.com>
2020-11-02 09:41:58 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,15 @@
 	var sluitenKnop = document.querySelector('#button');
 	var menu = document.querySelector("#navbar");
 
-function sluiten (){
-	menu.classList.remove('open');
-	menu.classList.add('dicht');
-}
 
 function openen(){
-	menu.classList.remove('dicht');
-	menu.classList.add('open');
+	menu.classList.toggle('open');
 }
+
 
 //eventlisteners
 
 	openKnop.addEventListener('click', openen);
-	sluitenKnop.addEventListener('click', sluiten)
 
 
 // bron: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar //
